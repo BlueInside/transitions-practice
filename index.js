bb682081459c8ee30761b38a126791c26f32b6e1
Karol <kpulawski13@gmail.com>
2023-09-27 12:40:13 +0100

    transitions practice flip card

---
+++ index.js
@@ -0,0 +1,6 @@
+const f = document.getElementById('foo');
+
+document.addEventListener('click', (e) => {
+  f.style.transform = `translateY(${e.clientY - 25}px)`;
+  f.style.transform += `translateX(${e.clientX - 25}px)`;
+});
